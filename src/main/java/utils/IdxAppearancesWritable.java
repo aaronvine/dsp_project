d5dab138b969f27968db1d3b891e36920a0d56ac
Aaron <aaronvine@gmail.com>
2016-08-13 15:49:18 +0300

    writables

---
+++ src/main/java/utils/IdxAppearancesWritable.java
@@ -0,0 +1,38 @@
+import org.apache.hadoop.io.*;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class IdxAppearancesWritable implements Writable {
+
+    public IntWritable idx;
+    public IntWritable numOfAppearances;
+
+    public IdxAppearancesWritable() {
+        this.idx = new IntWritable();
+        this.numOfAppearances = new IntWritable();
+    }
+
+    public IdxAppearancesWritable(IntWritable idx, IntWritable numOfAppearances) {
+        this.idx = idx;
+        this.numOfAppearances = numOfAppearances;
+    }
+
+    public IdxAppearancesWritable(int idx, int numOfAppearances) {
+        this(new IntWritable(idx), new IntWritable(numOfAppearances));
+    }
+
+    public void write(DataOutput dataOutput) throws IOException {
+        idx.write(dataOutput);
+        numOfAppearances.write(dataOutput);
+    }
+
+    public void readFields(DataInput dataInput) throws IOException {
+        idx.readFields(dataInput);
+        numOfAppearances.readFields(dataInput);
+    }
+
+    public String toString() {
+        return "(" + this.idx + "," + this.numOfAppearances + ")";
+    }
+}
---
+++ src/main/java/utils/PairWritable.java
@@ -0,0 +1,110 @@
+import org.apache.hadoop.io.*;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class PairWritable implements Writable, WritableComparable<PairWritable> {
+
+    public Text noun1;
+    public Text noun2;
+    public IntWritable numOfAppearances;
+    public BooleanWritable value;
+
+    public PairWritable() {
+        this.noun1 = new Text();
+        this.noun2 = new Text();
+        this.occurrences = new IntWritable(0);
+        this.value = new BooleanWritable(false);
+    }
+
+    public PairWritable(String noun1, String noun2, int numOfAppearances) {
+        this.noun1 = new Text(noun1);
+        this.noun2 = new Text(noun2);
+        this.occurrences = new IntWritable(numOfAppearances);
+        this.value = new BooleanWritable(false);
+    }
+
+    public PairWritable(String noun1, String noun2, Boolean value) {
+        this.noun1 = new Text(noun1);
+        this.noun2 = new Text(noun2);
+        this.numOfAppearances = new IntWritable(-1);
+        this.value = new BooleanWritable(value);
+    }
+
+    public void write(DataOutput dataOutput) throws IOException {
+        noun1.write(dataOutput);
+        noun2.write(dataOutput);
+        numOfAppearances.write(dataOutput);
+        value.write(dataOutput);
+    }
+
+    public void readFields(DataInput dataInput) throws IOException {
+        noun1.readFields(dataInput);
+        noun2.readFields(dataInput);
+        numOfAppearances.readFields(dataInput);
+        value.readFields(dataInput);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PairWritable pair = (PairWritable) o;
+        if (numOfAppearances != pair.numOfAppearances) {
+            return false;
+        }
+        if (noun1 != null ? !noun1.equals(pair.noun1) : pair.noun1 != null) {
+            return false;
+        }
+        if (noun2 != null ? !noun2.equals(pair.noun2) : pair.noun2 != null) {
+            return false;
+        }
+        return value == pair.value;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = numOfAppearances.hashCode();
+        result = 31 * result + (noun1 != null ? noun1.hashCode() : 0);
+        result = 31 * result + (noun2 != null ? noun2.hashCode() : 0);
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
+
+    public String toString() {
+        return noun1 + "," + noun2;
+    }
+
+
+    public IntWritable getType() {
+        if (numOfAppearances.get() < 0)
+            return new IntWritable(1);
+        else
+            return new IntWritable(2);
+    }
+
+    public int compareTo(NounPair o) {
+        int compareNoun1 = this.noun1.compareTo(o.noun1);
+        if (compareNoun1 == 0) {
+            int compareNoun2 = this.noun2.compareTo(o.noun2);
+            if (compareNoun2 == 0) {
+                if (this.getType().compareTo(o.getType()) == 0) {
+                    return 0;
+                } else {
+                    if (this.getType().get() == 1) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                }
+            }
+            return compareNoun2;
+        } else {
+            return compareNoun1;
+        }
+    }
+}
